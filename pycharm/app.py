4cf50cf3862368076adf7678efea01d2ab6e1fb2
Ruslan <83557897+ruslan-shulhan@users.noreply.github.com>
2021-08-31 16:26:53 -0400

    add flask files

---
+++ pycharm/app.py
@@ -0,0 +1,37 @@
+import pandas as pd
+import pickle
+from flask import Flask, jsonify, request
+
+
+# load model
+path = '../model/pickle_model/model.pkl'
+model = pickle.load(open(path, 'rb'))
+
+# app
+app = Flask(__name__)
+
+# routes
+@app.route('/', methods=['POST'])
+
+def predict():
+    # get data
+    data = request.get_json(force=True)
+
+    # convert data into dataframe
+    data.update( (x, [y]) for x, y in data.items() )
+    data_df = pd.DataFrame.from_dict(data)
+
+    # predictions
+    result = model.predict(data_df)
+
+    # send back to browser
+    output = {
+        'result': int(result[0])
+    }
+
+    # return data
+    return jsonify(result=output)
+
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
